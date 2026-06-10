b3cae9e55c33b874edf5dcb6b1ed5986ffb7b195
Sai Prakash <saiprakash1916@gmail.com>
2026-06-10 23:04:51 +0530

    Created the Header and footer components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import './styles.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Moviedux</h1>
-      </header>
-      <footer className="footer">
-        <p>Footer content</p>
-      </footer>
+      <div className='container'>
+        <Header></Header>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css"
+
+export default function Footer(){
+    const currentYear = new Date().getFullYear();
+    return(
+        <div className="footer">
+            <p className="footer-text">
+                © {currentYear} Moviedux, All rights reversed.
+            </p>
+        </div>
+    )
+}
