cda6d944fa12001dff4c3792cb5e95312e885605
Sai Prakash <saiprakash1916@gmail.com>
2026-06-10 22:21:50 +0530

    Intial setup for creating the react application

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './styles.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Welcome to Moviedux</h1>
+      </header>
+      <footer className="footer">
+        <p>Footer content</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
